cddeef949ba4ef7896a2e8917c1c2d2c4a9581be
therealsanisboring <msanisha01@gmail.com>
2022-05-20 22:32:15 +0545

    added day 4

---
+++ src/Day04/README.md
@@ -0,0 +1,3 @@
+![Output](./output1.png)
+![Output](./output2.png)
+![Output](./output3.png)
--- src/Day04/code.py
+++ src/Day04/code.py
@@ -1,6 +1,7 @@
 # OOP in python
 
 #Pclass and object
+
 class Student:
     def __init__(self, fname, lname, gender, age):
         self.fname = fname
@@ -16,3 +17,57 @@ s1 = Student("Chandan", "Shakya", "Male", 23)
 
 print(s1.email)
 print(s1.bio())
+
+
+# demonstrating the use of inheritance
+
+class BCA(Student):
+    def __init__(self, fname, lname, gender, age, progLang):
+        super().__init__(fname, lname, gender, age)
+        self.progLang = progLang
+    def detail(self):
+       return 'Name: {} {} \nGender: {}\nAge: {}\nprogLang: {}'.format(self.fname, self.lname, self.gender, self.age, self.progLang)
+
+
+class BHM(Student):
+    def __init__(self, fname, lname, gender, age, workArea):
+        super().__init__(fname, lname, gender, age)
+        self.workArea = workArea 
+    def info(self):
+       return 'Name: {} {} \nGender: {}\nAge: {}\nworkArea: {}'.format(self.fname, self.lname, self.gender, self.age, self.workArea)
+
+
+s2 = BCA("Kabir", "Deula", "Male", 24, "Dart")
+s3 = BHM("Jasmina", "Magar", "Female", 21, "House Keeping" )
+
+
+print(s2.detail())
+print(s3.info())
+
+
+# method overridding
+
+class University():
+    def __init__(self):
+        self.value = "Tribhuwan university"
+
+    def display(self):
+        print(self.value)
+
+class College(University):
+    def __init__(self):
+        super().__init__()
+        self.value = "NCCS"
+    
+    def display(self):
+        return super().display()
+
+obj1 = University()
+obj2 = College()
+
+obj1.display()
+obj2.display()
+        
+
+
+
