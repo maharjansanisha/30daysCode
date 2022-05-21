47bd4a50e3c9f6efc427e91f6dba4e570fce235d
therealsanisboring <msanisha01@gmail.com>
2022-05-22 00:10:16 +0545

    updated

--- src/Day05/code.py
+++ src/Day05/code.py
@@ -17,13 +17,38 @@ print(x)
 x = dir(platform)
 print(x)
 
+#Python Math
+
+#Built-in Math function
+
+x = min(7, 9, 26)       
+y = max(87, 45, 34)
+
+print(x)
+print(y)
+
+ab = abs(-453)  #returns the absoulte value of the given number
+print(ab)
+
+# Math Module
+
 import math 
 print(math.log(100))
 
 import random
 print(random.randrange(0, 10))  
 
+#Python Date 
 import datetime
 print(datetime.date.today())
 print(datetime.datetime.now())
 
+
+x = datetime.datetime.now()
+
+print(x.year)               #returns the year 
+print(x.strftime("%A"))     #returns the name of the weekday
+
+# creating a date object
+x = datetime.date(2002, 8, 21)
+print(x)
